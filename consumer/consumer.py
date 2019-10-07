9b51ea80cef36dff0570a052ac9b82aa56894318
root <root@ira.local>
2019-10-07 23:05:20 +0300

    work

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,18 +1,40 @@
 from kafka import KafkaConsumer
 import datetime
+import time
+import logging
+import os
+
+logging.basicConfig(filename='/tmp/consumer.log',level=logging.INFO)
 
 now=datetime.datetime.now()
 print ("Consumer started: %s" % str(now))
+logging.info('KAFKA_SRVS='+os.environ.get('KAFKA_SRVS'))
+logging.info("Consumer started: %s" % str(now))
+brocker_started=bool(False)
 
-# To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('my-topic2',
-                         group_id='my-group'+str(now),
-                         bootstrap_servers=['172.17.0.1:9092'],
-                         auto_offset_reset='earliest',
-                         enable_auto_commit=False)
+while brocker_started != True:
+    try:
+        consumer = KafkaConsumer('my-topic',
+                                 group_id='my-group'+str(now),
+                                 bootstrap_servers=[os.environ.get('KAFKA_SRVS')],
+                                 auto_offset_reset='earliest',
+                                 enable_auto_commit=False)
+    except Exception as e:
+        print("Brocker NOT started!")
+        logging.info("Brocker NOT started!" )
+        print(str(e))
+        time.sleep(5)
+    else:
+        brocker_started=bool(True)
+
+
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
+    logging.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                                message.offset, message.key,
+                                                message.value))
+
--- producer/producer.py
+++ producer/producer.py
@@ -1,22 +1,49 @@
 from kafka import KafkaProducer
 import datetime
 import time
+import os
+import logging
 
 
-producer = KafkaProducer(bootstrap_servers=['172.17.0.1:9092'])
+logging.basicConfig(filename='/tmp/producer.log',level=logging.INFO)
+brocker_started=bool(False)
+print(os.environ.get('KAFKA_SRVS'))
+logging.info('KAFKA_SRVS='+os.environ.get('KAFKA_SRVS'))
+
+while brocker_started != True:
+    try:
+        producer = KafkaProducer(bootstrap_servers=[os.environ.get('KAFKA_SRVS')],
+                                 max_block_ms=600000)
+#        producer = KafkaProducer(bootstrap_servers=[os.environ['kafka1']+'9092',os.environ['kafka2']+'9093',os.environ['kafka3']+'9094'])
+    except:
+        print("Brocker NOT available!" )
+	
+        logging.info('Brocker NOT available!')
+#        logging.exception(str(e))
+#        producer =''
+        time.sleep(10)
+    else:
+        brocker_started=bool(True)
+
+
+
 steps=100
-topic='my-topic2'
+topic='my-topic'
 for i in range(steps):
     now = str(datetime.datetime.now())
     if i == 0:
         print("started: %s" % str(now))
         producer.send(topic, key=b'foo', value=b'===========Producer started============'+bytes(now, encoding='utf8'))
+        logging.info(now+' ===========Producer started============')
     print(i)
     producer.send(topic, key=b'foo', value=bytes(now, encoding='utf8'))
+    logging.info(now+' '+str(i))
     if i == steps-1:
         print("stopped: %s" % str(now))
         producer.send(topic, key=b'foo', value=b'===========Producer stopped============'+bytes(now, encoding='utf8'))
+        logging.info(now + ' ===========Producer stopped============')
     time.sleep(1)
+
 
 
 
